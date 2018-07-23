b7a48665d559902afbde64475251660d4c1163d0
aj <sethi.developer1506@gmail.com>
2018-07-24 00:46:23 +0530

    comma missing

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -25,7 +25,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '9839152090',
                 'email' => 'avijais@yahoo.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -36,7 +36,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777770',
                 'email' => 'admin1@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -47,7 +47,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777771',
                 'email' => 'admin2@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -58,7 +58,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777772',
                 'email' => 'teacher1@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -69,7 +69,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777773',
                 'email' => 'teacher2@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -80,7 +80,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777774',
                 'email' => 'teacher3@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -91,7 +91,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777775',
                 'email' => 'executive1@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -102,7 +102,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777776',
                 'email' => 'executive2@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -113,7 +113,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777777',
                 'email' => 'executive3@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -124,7 +124,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777778',
                 'email' => 'executive4@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -135,7 +135,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777779',
                 'email' => 'student1@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -146,7 +146,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777780',
                 'email' => 'student2@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -157,7 +157,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777781',
                 'email' => 'student3@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -168,7 +168,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777782',
                 'email' => 'student4@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -179,7 +179,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777783',
                 'email' => 'student5@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -190,7 +190,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777784',
                 'email' => 'admin31@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -201,7 +201,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777785',
                 'email' => 'admin32@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -212,7 +212,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777786',
                 'email' => 'teacher31@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -223,7 +223,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777787',
                 'email' => 'teacher32@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -234,7 +234,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777788',
                 'email' => 'teacher33@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -245,7 +245,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777789',
                 'email' => 'executive31@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -256,7 +256,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777790',
                 'email' => 'executive32@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -267,7 +267,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777791',
                 'email' => 'executive33@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -278,7 +278,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777792',
                 'email' => 'executive34@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -289,7 +289,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777793',
                 'email' => 'student31@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -300,7 +300,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777794',
                 'email' => 'student32@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -311,7 +311,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777795',
                 'email' => 'student33@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -322,7 +322,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777796',
                 'email' => 'student34@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],[
@@ -333,7 +333,7 @@ class UsersTableSeeder extends Seeder
                 'mobile' => '0077777797',
                 'email' => 'student35@email.com',
                 'password' => bcrypt('123456'),
-                'verified' => 1
+                'verified' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ]
