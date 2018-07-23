eae74b8ce83a5a5654e5787c4ffd3dbf93ad896e
aj <sethi.developer1506@gmail.com>
2018-07-23 23:52:23 +0530

    Created models directroy and added Models [client, clss, Role, Section and User)

---
+++ app/Models/Clss.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Clss extends Model
+{
+    protected $table = 'Classes';
+    // public function section() {
+    // 	return $this->belongsToMany('Classes');
+    // }
+}
