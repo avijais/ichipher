99bf508d6f57a25433df493af4edba1cd44e9f88
aj <sethi.developer1506@gmail.com>
2018-10-01 00:49:23 +0530

    set user data to each request

--- app/Http/Middleware/CheckApiToken.php
+++ app/Http/Middleware/CheckApiToken.php
@@ -16,19 +16,33 @@ class CheckApiToken
      */
     public function handle($request, Closure $next)
     {
-        if ($request->headers->has('api_token') && $request->headers->has('user_id')) {
+        if ($request->headers->has('api_token')) {
+        // if ($request->headers->has('api_token') && $request->headers->has('user_id')) {
             // CAN USE FOLLOWING WAY TO ACCESS HEADER DATA
             // $apiToken = $request->headers->get('api_token');
             // $userId = $request->headers->get('user_id');
 
+            // $userId = Request::header('user_id');
+
             $apiToken = Request::header('api_token');
-            $userId = Request::header('user_id');
+            $isUserExist = User::where(['api_token' => $apiToken])->first();
+            
+            // $isUserExist = User::where(['api_token' => $apiToken, 'id' => $userId])->first();
 
-            $model = User::where(['api_token' => $apiToken, 'id' => $userId])->first();
-
-            if (!$model) {
+            if (!$isUserExist) {
                 return response(['status' => 'error', 'message' => 'Authentication faild!!']);
             }
+
+            $userData = [
+                'is_loggedin' => true,
+                'id' => $isUserExist->id,
+                'client_id' => $isUserExist->client_id,
+                'fname' => $isUserExist->fname,
+                'lname' => $isUserExist->lname,
+                'mobile' => $isUserExist->mobile,
+            ];
+            $request->session()->put('userData', $userData);
+
             return $next($request);
         } else {
             return response(['status' => 'error', 'message' => "Please send required data for authentication!!"]);
